6944981648d7b146224acd50371ca4c5f94e034d
Maarten Saliën <34309400+maartentje@users.noreply.github.com>
2024-05-05 14:09:02 +0200

    renaming

--- apps/mscmdclock/cmd-clock-icon.js
+++
@@ -1,1 +0,0 @@
-require("heatshrink").decompress(atob("mEwgImjv//AAX+AotggEBAoPmAonGuAFD9wdLCIIAChwdBAAUGAqVAAongAogqDAoM4AokeAokPAokHwAFDgfAAocBFQQLHg4dEhwFEjgFEvAFE+AFDsPgn/+BYXAAoh9XAoitLfcYA="))
