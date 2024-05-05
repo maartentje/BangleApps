5bf375efaeb8cd79cab3f82c169ea87a2cb731d4
Maarten Saliën <34309400+maartentje@users.noreply.github.com>
2024-05-05 13:58:32 +0200

    Create cmd-clock-icon.js

---
+++ apps/mscmdclock/cmd-clock-icon.js
@@ -0,0 +1,1 @@
+require("heatshrink").decompress(atob("mEwgImjv//AAX+AotggEBAoPmAonGuAFD9wdLCIIAChwdBAAUGAqVAAongAogqDAoM4AokeAokPAokHwAFDgfAAocBFQQLHg4dEhwFEjgFEvAFE+AFDsPgn/+BYXAAoh9XAoitLfcYA="))
